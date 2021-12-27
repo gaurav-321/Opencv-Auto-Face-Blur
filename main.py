a75d7703521c1ff66517755c8f8fe43df84ef52a
gaurav verma <gauravverma64786@gmail.com>
2021-12-27 23:16:28 +0530

    Update

--- main.py
+++ main.py
@@ -1,43 +1,43 @@
+import traceback
+
 import cv2
-import sys
-from vidgear.gears import WriteGear
 
+input_file = r"test/test_1.mp4"
 cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
-output_params = {"-vcodec": "h264", "-crf": 0,
-                 "-preset": "fast"}  # define (Codec,CRF,preset) FFmpeg tweak parameters for writer
-video_capture = cv2.VideoCapture("test/test_1.mp4")
-last = []
-
-writer = WriteGear(output_filename='output/Output.mp4', compression_mode=True, logging=True, **output_params)
-while video_capture.read()[0]:
+SCALE = 0.2  # lowering it increase the performance but decrease accuracy
+RATIO = round(1/SCALE) # lowering it increase the performance but decrease accuracy
+cap = cv2.VideoCapture(input_file)
+ret, frame = cap.read()  # Get one ret and frame
+h, w, _ = frame.shape  # Use frame to get width and height
+fourcc = cv2.VideoWriter_fourcc(*"XVID")
+writer = cv2.VideoWriter("output/Output.mp4", fourcc, 24, (w, h))
+faces_loc = []
+while ret:
     try:
-        ret, frame = video_capture.read()
-        frame = cv2.resize(frame, (0, 0), fx=0.2, fy=0.2)
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
+        ret, frame = cap.read()
+        frame_small = cv2.resize(frame, (0, 0), fx=SCALE, fy=SCALE)
         faces = cascade.detectMultiScale(
-            gray,
-            scaleFactor=1.1,
-            minNeighbors=4,
+            cv2.cvtColor(frame_small, cv2.COLOR_BGR2GRAY),
+            scaleFactor=1.05,
+            minNeighbors=3,
             minSize=(60, 60)
         )
-        if len(faces) == 0:
-            faces = [last[-1]]
-        for cord in faces:
-            (x, y, w, h) = cord
-            last.append(cord)
-            sub_face = frame[y:y + h, x:x + w]
-            sub_face = cv2.GaussianBlur(sub_face, (23, 23), 30)
-            frame[y:y + sub_face.shape[0], x:x + sub_face.shape[1]] = sub_face
 
-        cv2.imshow('Video', frame)
+        if len(faces) == 0 and len(faces_loc) > 2:
+            faces = faces_loc[-2:]
+        for (x, y, w, h) in faces:
+            faces_loc.append((x, y, w, h))
+            sub_face = frame[y * RATIO:y * RATIO + h * RATIO, x * RATIO:x * RATIO + w * RATIO]
+            sub_face = cv2.blur(sub_face, (50, 50))
+            frame[y * RATIO:y * RATIO + h * RATIO, x * RATIO:x * RATIO + w * RATIO] = sub_face
+        frame_small = cv2.resize(frame, (0, 0), fx=SCALE, fy=SCALE)
+        cv2.imshow('Video', frame_small)
         writer.write(frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-    except:
+    except Exception as e:
         pass
 
-# When everything is done, release the capture
-video_capture.release()
+cap.release()
 cv2.destroyAllWindows()
-writer.close()
+writer.release()
